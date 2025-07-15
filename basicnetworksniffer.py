df1b341999a56341fd31969c673de989f7215ca5
YaminiAsole20 <yaminiasole@gmail.com>
2025-07-15 14:23:27 +0530

    Update basicnetworksniffer.py

--- basicnetworksniffer.py
+++ basicnetworksniffer.py
@@ -1,1 +1,12 @@
 
+from scapy.all import sniff
+
+def process_packet(packet):
+    print(packet.summary())
+
+print("Starting packet capture...")
+sniff(
+    prn=process_packet,
+    count=0,  
+    store=False   
+)
